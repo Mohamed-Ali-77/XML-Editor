6b3964abf30702b611e23afbe687e556e0ed24a4
ao <abdullahosama15@gmail.com>
2021-12-02 10:51:27 +0200

    Adding comments and making small changes

--- XMLTree.h
+++ XMLTree.h
@@ -4,10 +4,13 @@
 #include<vector>
 #include<string>
 #include<stack>
-#include<iostream>
-
+#include<sstream>
 using namespace std;
 
+
+// Node for XML tag containing a vector its children nodes
+// Children represents the inner tags encapsulated by tag that the node represents
+// Name is the tag name and if the node doesn't have children then represents the text gata of the tag
 class Node
 {
 private:
@@ -19,7 +22,7 @@ public:
     Node(string tagName = "") {
         name = tagName;
     }
-    
+
     bool hasChildren() {
         return numberOfChilds() != 0;
     }
@@ -39,9 +42,9 @@ public:
         return name;
     }
 
-
 };
 
+// Constructing a tree from XML
 class XMLTree
 {
 
@@ -50,25 +53,37 @@ private:
     string body;
     int currentOpeningTag = -1;
     int currentClosingTag;
+
 public:
+
     XMLTree(const string& xmlContent);
 
-    int getNextOpening() {
-        currentOpeningTag++;
+    // Returns string with XML inorder
+    string inorderXML();
+    
 
-        return currentOpeningTag = body.find_first_of("<", currentOpeningTag);
+    Node* getRoot() {
+        return root;
     }
 
+    ~XMLTree();
+
+private:
+    // Gets position for the next "<"
+    int getNextOpening() {
+        currentOpeningTag++;
+        return currentOpeningTag = body.find_first_of("<", currentOpeningTag);
+    }
+    // Gets position for the ">" which comes after the currentOpeningTag 
     int getClosing() {
         return currentClosingTag = body.find_first_of(">", currentOpeningTag);
     }
-
+    // Checks if the tag that starts at currentOpeningTag is a closing Tag or not
     bool isClosingTag() {
         return body[currentOpeningTag + 1] == '/';
     }
 
-
-    ~XMLTree();
+    void inorderXML(Node* tag, stringstream& str);
 
 };
 
@@ -85,27 +100,25 @@ XMLTree::XMLTree(const string& xmlContent)
         getClosing();
 
         if (isClosingTag()) {
-            // cout << "Popped: " << depth.top() -> getName() << " " << depth.top() -> numberOfChilds() << endl;
 
             // If Node doesn't have children then it is a leaf node
             if (!depth.top() -> hasChildren()) {
                 string textData = body.substr(prevClosingTag + 1, currentOpeningTag - prevClosingTag - 1);
                 Node* textNode = new Node(textData);
                 depth.top() -> addChild(textNode);
-                // cout << textData << endl;
             }
             depth.pop();
             continue;
         };
 
-        // cout << currentOpeningTag << endl;
-        // cout << currentClosingTag << endl;
-
+        // Gets name of the tag
         string currentTagName = body.substr(currentOpeningTag + 1, getClosing() - currentOpeningTag - 1);
+        int firstSpace = currentTagName.find_first_of(" ");
+        if (firstSpace != string::npos) currentTagName.erase(firstSpace);
+        
         Node* currentTag = new Node(currentTagName);
         depth.top() -> addChild(currentTag);
         depth.push(currentTag);
-        // cout << "Pushed: "<< depth.top() -> getName()  << endl;
 
         prevClosingTag = currentClosingTag;
     }
@@ -113,6 +126,29 @@ XMLTree::XMLTree(const string& xmlContent)
 }
 
 
+string XMLTree::inorderXML() {
+    stringstream str;
+    for(int i = 0; i < root -> numberOfChilds(); ++i) {
+        inorderXML(root -> getChild(i), str);
+    }
+    return str.str();
+
+}
+
+void XMLTree::inorderXML(Node* tag, stringstream& str) {
+    if(!tag -> hasChildren()) {
+        str << tag -> getName();
+        return;
+    }
+
+    str << "<" << tag -> getName() << ">"; 
+
+    for(int i = 0; i < tag -> numberOfChilds(); ++i) {
+        inorderXML(tag -> getChild(i), str);
+    }
+
+    str << "</" << tag -> getName() << ">";
+}
 
 XMLTree::~XMLTree()
 {
