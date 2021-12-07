a68ed67546abda621d8b92d32693be06de8949bf
Ahmed Essam <85046148+AhmedEssam262@users.noreply.github.com>
2021-12-07 14:09:59 +0200

    Update XM:_Correction.cpp

--- XM:_Correction.cpp
+++ XM:_Correction.cpp
@@ -3,66 +3,78 @@
 #include <fstream>
 using namespace std;
 #include<string>
-
-// Function to remove all spaces from a given string
 string removeSpaces(string str)
 {
     str.erase(remove(str.begin(), str.end(), ' '), str.end());
     return str;
 }
-string ReadTXT(string F_NAME) {
-    fstream file(F_NAME.c_str());   // sample.xml
-    string s, ss = "";
-    if (file.is_open()) {
-        string line;
-        while (getline(file, line)) {
-            // using printf() in all tests for consistency
-            s = line.c_str();
-           // s = removeSpaces(s);
 
-            return s;
+
+void inline_C(string &s) {
+     s = removeSpaces(s);
+
+    int close1 = s.find(">");
+    int open1 = s.find("<");
+
+    if ((s.length() > close1 + 1) && open1 != -1) {
+        int open1 = s.find("<");
+      int open2 = s.find("<",2);
+      if (open2 == -1) {
+          string s2 = s +'<'+'/'+ "         ";
+          int close1 = s.find(">");
+          open2 = s2.find("<", 2);
+          //   s2[open2 + 1] = '/';
+
+          while (open1 < close1) {
+              s2[open2 + 2] = s2[open1 + 1];
+              open1++;
+              open2++;
+          }
+          s = s2;
+      }
+        
+        
+      else {
+          string s2 = s + "         ";
+
+          int close1 = s.find(">");
+          open2 = s2.find("<", 2);
+          //   s2[open2 + 1] = '/';
+      
+          while (open1 < close1) {
+              s2[open2 + 2] = s2[open1 + 1];
+              open1++;
+              open2++;
+          }
+          s = s2;
         }
-        file.close();
     }
 }
 
-int main() {
-    /*
-    ifstream file("sample.xml");
-    string s, ss = "";
+string ReadTXT(string F_NAME) {
+    fstream file(F_NAME.c_str());   // sample.xml
+    string s,ss = "";
     if (file.is_open()) {
         string line;
         while (getline(file, line)) {
             // using printf() in all tests for consistency
             s = line.c_str();
-          //  s = removeSpaces(s);
-
-            cout << s << endl;
-          //  cout << "*************************************************\n";
+            inline_C(s);
+            //cout << s << endl;
+            ss += s;
 
         }
         file.close();
+        return ss;
     }
-    cout << endl;
-    cout << "*************************************************\n";
-    */
-  //Inline Errors
-    string s = "<name>a";
-    int close1 = s.find(">");
-    if (s.length() > close1+1) {
-        string s2 = s + s[0]+'\\'+"         ";
-        int open1=s.find("<");
-        int close1 = s.find(">");
-        int open2 = s2.find("<", 2);
-        while (open1 < close1) {
-            s2[open2 + 2] = s2[open1 + 1];
-            open1++;
-            open2++;
-        }
-        s = s2;
-    }
-    cout << s << endl;
+}
 
+
+int main() {
+    cout << ReadTXT("sample.xml")<<endl;
+    string x = "<And>";
+    inline_C(x);
+    //cout << x;
 
 }
 
