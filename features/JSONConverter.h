7aeec638ab2ae57c85f0fba4203acc8efb7b0e08
ao <abdullahosama15@gmail.com>
2021-12-09 02:17:01 +0200

    Handling array case

--- features/JSONConverter.h
+++ features/JSONConverter.h
@@ -29,30 +29,54 @@ JSONConverter::JSONConverter(XMLTree& t): tree(t) {
 void JSONConverter::convert() {
     Node* root = tree.getRoot();
     stringstream str;
-    str << "{\n";
-    convert(root -> getChild(0), TABS, str);
-    str << "\n}";
+    convert(root, "", str);
     jsonStr = str.str();
 }
 bool hasLeafChild(Node* node) {
     return node -> numberOfChilds() == 1 && !node -> getChild(0)->hasChildren();
 }
+bool hasArray(Node* node) {
+    if(node -> numberOfChilds() > 1) 
+        if(node -> getChild(0) -> getName() == node -> getChild(1) -> getName()) return true;
+    
+    return false;
+}
+bool escapedChars(const char& c) {
+    if (c == '\n' || c == '\r' || c == ' ') return true;
+    else return false;
+}
+void escapeString(const string& sentence, stringstream& ss) {
+    int start = 0, end = sentence.size() - 1;
+    while(escapedChars(sentence[start])) start++;
+    while(escapedChars(sentence[end])) end--;
+    for (int i = start; i <= end; ++i) {
+        ss << sentence[i]; 
+    }
+}
+
 void JSONConverter::convert(Node* node, string indent, stringstream& ss) {
 
-    ss << indent << "\"" <<  node -> getName() << "\": ";
-    if (!hasLeafChild(node)) ss << "{\n";
+    if (hasArray(node)) ss << "[\n" << indent + TABS;
+    else if (!hasLeafChild(node)) ss << "{\n";
 
     for(int i = 0; i < node ->numberOfChilds(); ++i) {
 
         if(!node -> getChild(i) -> hasChildren()) {
-            ss << "\"" << node -> getChild(i) -> getName() << "\"";
+            ss << "\"", escapeString(node -> getChild(i) -> getName(), ss), ss << "\"";
+        }else {
+
+            if(!hasArray(node)) 
+                ss << indent + TABS << "\"" << node -> getChild(i) -> getName() << "\": ";
+            convert(node -> getChild(i), indent + TABS, ss);
+
         }
-        else convert(node -> getChild(i), indent + TABS, ss);
         if(i < node -> numberOfChilds() - 1) ss << ",\n";
+        if(hasArray(node)) ss << indent + TABS;
 
     }
 
-    if (!hasLeafChild(node)) ss << "\n" << indent << "}";
+    if(hasArray(node)) ss << "\n" << indent << "]";
+    else if (!hasLeafChild(node)) ss << "\n" << indent << "}";
 
 }
 
