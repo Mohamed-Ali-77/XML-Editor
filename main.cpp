562150d8076be7f40ef19c8b94b2cc22eb0b4904
Mohamed Ali <85204433+Mohamed-Ali-77@users.noreply.github.com>
2021-12-10 22:53:21 +0200

    Add Funcations That produce File from vector of strings

--- main.cpp
+++ main.cpp
@@ -17,3 +17,42 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
+void Output_File( const vector<string> &XML_string, int size)	  // For JSON File
+{
+    fstream newfile;
+    newfile.open("JSONfile.json",ios::out);
+    if(newfile.is_open())
+    {
+        for (int i = 0; i < size; i++)
+        {
+            newfile << XML_string[i] << "\n";
+        }
+    }
+}
+
+void Output_File(const vector<string> &Spaces, const vector<string> &XML_string, int size, string caller)
+{
+    fstream newfile;
+    string fname;
+    if(caller == "Format"){
+        fname = "Format.xml";
+    }
+    else if(caller == "Decompress")
+        {
+            fname = "Decompress.xml";
+        }
+    else if(caller == "Minified"){
+        fname = "Minified.xml";
+    }
+    else if (caller == "Fixed"){
+        fname = "Fixed.xml";
+    }
+    newfile.open(fname,ios::out);
+    if(newfile.is_open())
+    {
+        for (int i = 0; i < size; i++)
+        {
+            newfile << Spaces[i] << XML_string[i] << "\n";
+        }
+    }
+}
