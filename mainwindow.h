7cd823dc9df4e0a921fc7b1863b4e447ace0f990
Mohamed Ali <85204433+Mohamed-Ali-77@users.noreply.github.com>
2021-12-10 22:50:39 +0200

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -46,9 +46,23 @@ private slots:
 
     void on_actionRedo_triggered();
 
-    void on_pushButton_clicked();      //Check Correctness
+    void on_pushButton_clicked();               //Check Correctness
 
-    void on_pushButton_2_clicked();        //Compress file
+    void on_pushButton_2_clicked();             //Compress file
+    
+    void on_pushButton_clicked();               //Check Correctness
+
+    void on_pushButton_2_clicked();             //Compress file
+
+    void on_pushButton_3_clicked();             //Decompress file
+
+    void on_pushButton_4_clicked();             //Minifying
+
+    void on_pushButton_5_clicked();             /*Convert to JSON*/
+
+    void on_pushButton_6_clicked();             /*Formatting (Prettifying)*/
+
+    void on_pushButton_7_clicked();             /* Detect for errors*/
 
 private:
     Ui::MainWindow *ui;
