c9a3ab4eee1d87360f3b36b36f76ed9b28c3ecb0
Ahmed Magdy <85253814+Ahmed5920@users.noreply.github.com>
2021-12-11 14:45:14 +0200

    Create Detection.h

---
+++ Header Files/Detection.h
@@ -0,0 +1,26 @@
+#ifndef XML_DETECTION_H
+#define XML_DETECTION_H
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <assert.h>
+#include <cassert>
+#include <fstream>
+
+using namespace std;
+
+
+string ReadTXT(string F_NAME);
+bool Detection(string str);
+
+
+
+
+
+
+
+
+
+
+#endif // XML_DETECTION_H
