0f337aad80194ddd00a3d7ba0d7ec2f73b109fc1
Ahmed Magdy <85253814+Ahmed5920@users.noreply.github.com>
2021-12-15 21:05:09 +0200

    Update XML Detection.cpp

--- Sources/XML Detection.cpp
+++ Sources/XML Detection.cpp
@@ -2,8 +2,6 @@
 #include <string>
 #include <stack>
 #include <vector>
-#include <assert.h>
-#include <cassert>
 #include <fstream>
 
 using namespace std;
@@ -14,70 +12,96 @@ string removeSpaces(string str)
     return str;
 }
 
-string ReadTXT(string F_NAME) {
+
+
+bool Detection(string F_NAME) {
     fstream file(F_NAME.c_str());   // sample.xml
     string s = "";
-    string sum = "";
+    int close1, open1, close2, open2;
+    string str1 = "", str2 = "";
+    int numberofline = 1;
+    stack<int> n;           // stack for lines numbers that have mistakes
+    stack<string> stack1;
+    bool a;
     if (file.is_open()) {
         string line;
         while (getline(file, line)) {
             // using printf() in all tests for consistency
+            n.push(numberofline);
             s = removeSpaces(line.c_str());
-            sum += s;
+
+            close1 = s.find(">");
+            open1 = s.find("<");
+            close2 = s.find(">", close1 + 1);
+            open2 = s.find("<", open1 + 1);
+
+            if (open1 != -1 && close1 != -1 && open2 == -1)         // case open tag only in line
+            {
+                if (s[1] != '/') {
+                    str1.clear();
+                    while (open1 < close1 - 1) {
+                        str1 += s[open1 + 1];
+                        open1++;
+                    }
+                    stack1.push(str1);
+                }
+                else {
+                    str2.clear();
+                    while (open1 < close1 - 2) {
+                        str2 += s[open1 + 2];
+                        open1++;
+                    }
+                    if (str2 == stack1.top()) {
+                        stack1.pop();
+                        n.pop();
+                        n.pop();
+                    }
+                }
+
+
+            }
+
+            if (open1 != -1 && close1 != -1 && open2 != -1 && close2 != -1)         // case open and close tag in one line
+            {
+                str1.clear();
+                while (open1 < close1 - 1) {
+                    str1 += s[open1 + 1];
+                    open1++;
+                }
+                stack1.push(str1);
+                str2.clear();
+                while (open2 < close2 - 2) {
+                    str2 += s[open2 + 2];
+                    open2++;
+                }
+                if (str2 == stack1.top()) {
+                    stack1.pop();
+                    n.pop();
+                }
+            }
+
+            numberofline++;
+        }
+
+        while (!n.empty()) {
+            cout << n.top() << endl;
+            n.pop();
+        }
+
+        if (stack1.empty()) {
+            a = 0;           // no errors
+        }
+        else {
+            a = 1;           //error
         }
         file.close();
     }
-    return sum;
+    return a;
 }
 
-bool Detection(string str) {
-    stack<char> s1;
-    stack<char> s2;
-    vector<char> v1;
-    vector<char> v2;
-    s1.push('a');
-    for (int i = 0; i < str.length(); i++) {
-        if (i > 0) {
-            if (str[i] == '<' && s1.top() == '>') {
-                s1.pop();
-            }
-        }
-        if (str[i] == '<') {
-            s1.push(str[i]);
-            s2.push(str[i]);
-        }
-        if (s1.top() == '<' && s1.top() != '>' && str[i] != '<' && str[i] != '>' && str[i] != '/') {
-            v1.push_back(str[i]);
-        }
-        if (s1.top() == '/' && str[i] != '>') {
-            v2.push_back(str[i]);
-        }
-        if (str[i] == '>' && s1.top() != '/') {
-            s1.pop();
-            s1.push(str[i]);
-        }
-        if (str[i] == '>' && s1.top() == '/') {
-            s1.pop();
-            s1.pop();
-        }
-        if (str[i] == '/' && s2.top() == '<') {
-            assert(s2.size() >= 2 && "it can't clsoe tag come before open tag </><>");
-            s1.push(str[i]);
-            s2.pop();
-            s2.pop();
-        }
-    }
-    s1.pop();
 
-    if (s1.empty() && s2.empty() && (v1.size() == v2.size())) {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
+
 
 int main() {
-    string sum = ReadTXT("sample.xml");
-    cout << Detection(sum);
+    cout << Detection("sample.xml");
 }
