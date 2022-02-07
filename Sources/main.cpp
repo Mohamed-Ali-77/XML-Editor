00225af761e9eacc2b60ca68f838b80ab4b22d35
Mohamed Ali <85204433+Mohamed-Ali-77@users.noreply.github.com>
2022-02-07 18:48:15 +0200

    Add Function to Plot Social Network In GUI

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -65,3 +65,25 @@ void Output_File(const vector<string> &Spaces, const vector<string> &XML_string,
         }
     }
 }
+
+void Graphfile(SocialGraph& Graph){
+
+      ofstream myfile("graph.dot");
+      string Colors[] = {"[color=blue]\n","[color=red]\n","[color=green]\n"};
+
+      myfile<<"digraph{\n";
+      myfile<<"bgcolor=\"lightyellow\"\n";
+
+      for(int i =1; i<Graph.usersSocialNodes.size(); i++){
+          SocialNode* user = Graph.usersSocialNodes[i];
+                  myfile << user->id<< Colors[i-1];
+                  myfile << user->id<<"-> {";
+            for(int k = 0; k<user->followers.size(); k++){
+                myfile<<user->followers[k]<<" ";
+            }
+            myfile<<"}\n";
+      }
+      myfile<<"}";
+
+      myfile.close();
+}
